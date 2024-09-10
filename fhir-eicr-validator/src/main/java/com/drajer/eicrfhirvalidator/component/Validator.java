72daaa0a3674e00d794c04380b5d314655da8034
bhargav <bhargav.chowdhary@xyramsoft.com>
2024-09-11 03:46:18 +0530

    latest validator fixes

--- fhir-eicr-validator/src/main/java/com/drajer/eicrfhirvalidator/component/Validator.java
+++ fhir-eicr-validator/src/main/java/com/drajer/eicrfhirvalidator/component/Validator.java
@@ -10,13 +10,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -47,42 +54,57 @@ public class Validator {
             logger.info("Definitions:::::{}", definitions);
             final String txServer = true ? "http://tx.fhir.org" : null;
             final String fhirVersion = "4.0.1";
+            
+            System.setProperty("user.home", terminologycachePath.toString());
+			Path cachePath = Paths.get(terminologycachePath+"/.fhir/packages");
+			Files.createDirectories(cachePath);
 
-            FilesystemPackageCacheManager cacheManager =
-                    new FilesystemPackageCacheManager(
-                            FilesystemPackageCacheManager.FilesystemPackageCacheMode.USER);
-            String path = this.getClass().getClassLoader().getResource("packages").getPath().toString();
-            File packagePath = new File(path);
-            List<String> loaderSrcs = new ArrayList<>();
-            if (packagePath.exists() && packagePath.isDirectory() && packagePath.listFiles().length > 0) {
-                for (File file : packagePath.listFiles()) {
-                    String fullName = file.getName();
-                    String fileName = fullName.substring(0, fullName.lastIndexOf("."));
-                    String[] parts = fullName.split("\\W+");
-                    String version = "";
-                    for (int i = 0; i < parts.length - 1; i++) {
-                        if (parts.length - i <= 4) {
-                            version += parts[i] + ".";
-                        }
-                    }
-                    version = version.substring(0, version.length() - 1);
-                    String packageName = fileName.replace(version, "");
-                    packageName = packageName.substring(0, packageName.length() - 1);
-                    cacheManager.addPackageToCache(
-                            packageName, version, new FileInputStream(file), packageName);
-                    loaderSrcs.add(packageName + "#" + version);
-                }
-            }
+			FilesystemPackageCacheManager cacheManager = new FilesystemPackageCacheManager(
+					FilesystemPackageCacheManager.FilesystemPackageCacheMode.USER);
+			String path1 = this.getClass().getClassLoader().getResource("packages").getPath().toString();
+			String path = new ClassPathResource("packages").getURI().toString();
+			Resource[] resources = new PathMatchingResourcePatternResolver().getResources("classpath:/packages/*");
+			File packagePath = new File(path);
+			List<String> loaderSrcs = new ArrayList<>();
 
-            logger.info("initializing hl7Validator inside  Validator");
-            ValidationEngine validationEngine = getValidationEngine(definitions, path, true, fhirVersion, cacheManager,terminologycachePath);
-            logger.info("Done initializing");
+			Arrays.stream(resources).parallel().forEach(resource -> {
+				if (resource.exists() && resource.isReadable()) {
+					try (InputStream is = resource.getInputStream()) {
+						String fullName = resource.getFilename();
+						String fileName = fullName.substring(0, fullName.lastIndexOf("."));
+						String[] parts = fullName.split("\\W+");
+						String version = "";
+						for (int i = 0; i < parts.length - 1; i++) {
+							if (parts.length - i <= 4) {
+								version += parts[i] + ".";
+							}
+						}
+						version = version.substring(0, version.length() - 1);
+						String packageName = fileName.replace(version, "");
+						packageName = packageName.substring(0, packageName.length() - 1);
+						cacheManager.addPackageToCache(packageName, version, is, packageName);
+						loaderSrcs.add(packageName + "#" + version);
 
+					} catch (Exception e) {
+						logger.error("Error loading resource: " + resource.getFilename(), e);
+					}
+				}
+			});
 
-            IgLoader igLoader = new IgLoader(cacheManager, validationEngine.getContext(), validationEngine.getVersion());
-            for (String loaderSrc : loaderSrcs) {
-                igLoader.loadIg(validationEngine.getIgs(), validationEngine.getBinaries(), loaderSrc, false);
-            }
+			logger.info("Initializing HL7 Validator inside Validator");
+			ValidationEngine validationEngine = getValidationEngine(definitions, null, true, fhirSpecVersion,
+					cacheManager, terminologycachePath);
+			logger.info("Done initializing");
+
+			IgLoader igLoader = new IgLoader(cacheManager, validationEngine.getContext(),
+					validationEngine.getVersion());
+			loaderSrcs.parallelStream().forEach(loaderSrc -> {
+				try {
+					igLoader.loadIg(validationEngine.getIgs(), validationEngine.getBinaries(), loaderSrc, false);
+				} catch (Exception e) {
+					logger.error("Error loading IG: " + loaderSrc, e);
+				}
+			});
 
             validationEngine.connectToTSServer(txServer, null, FhirPublication.R4);
             validationEngine.setAnyExtensionsAllowed(true);
