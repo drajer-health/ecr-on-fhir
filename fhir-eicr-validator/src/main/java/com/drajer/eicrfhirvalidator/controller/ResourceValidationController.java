bf8ef8b3f7895e64002e11dacd9bfcaa9c9fb9c5
Arun Kumar <arunkumarhk@gmail.com>
2024-09-17 12:58:51 -0400

    Updated $validate

--- fhir-eicr-validator/src/main/java/com/drajer/eicrfhirvalidator/controller/ResourceValidationController.java
+++ fhir-eicr-validator/src/main/java/com/drajer/eicrfhirvalidator/controller/ResourceValidationController.java
@@ -98,4 +98,65 @@ public class ResourceValidationController {
         logger.info("Before Return");
         return new ResponseEntity<>(output, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+	    /**
+     * Validates r4 resources
+     *
+     * @param bodyStr
+     * @return
+     */
+    @PostMapping(value = "/fhir/Bundle/$validate")
+    public ResponseEntity<?> validateR4Bundle(@RequestBody String bodyStr,
+                                                @OptionalParam(name = "profile") String profile) throws Exception {
+        String output = null;
+        FhirValidator validator = r4Context.newValidator();
+        Resource resource = (Resource) r4Context.newJsonParser().setPrettyPrint(true).parseResource(bodyStr);
+
+        if(profile == null){
+            if(resource.hasMeta()) {
+                Meta resMeta = resource.getMeta();
+                if(resMeta.hasProfile()) {
+                    CanonicalType canonicalProfileType =resMeta.getProfile().get(0);
+                    profile = canonicalProfileType.asStringValue();
+                }
+            }
+        }
+
+        if(profile != null){
+            try {
+                String results;
+                logger.info("Validating with Profile : "+ profile);
+                OperationOutcome oo = validationService.validate(bodyStr, profile);
+                results = r5Context.newJsonParser().setPrettyPrint(true).encodeResourceToString(oo);
+                return new ResponseEntity<>(results, HttpStatus.OK);
+
+            } catch (Exception e) {
+                org.hl7.fhir.r4.model.OperationOutcome outcomes = new org.hl7.fhir.r4.model.OperationOutcome();
+                outcomes.addIssue().setSeverity(org.hl7.fhir.r4.model.OperationOutcome.IssueSeverity.ERROR)
+                        .setDiagnostics("Failed to parse request body as JSON resource. Error was: " + e.getMessage());
+                output = r4Context.newJsonParser().setPrettyPrint(true).encodeResourceToString(outcomes);
+            }
+        }else{
+            try {
+                ValidationResult results = validationService.validateR4Resource(r4Context, validator, bodyStr);
+                if (results instanceof ValidationResult && results.isSuccessful()) {
+                    logger.info("Validation passed");
+                } else {
+                    logger.error("Failed to validateR4Resource.");
+                }
+                org.hl7.fhir.r4.model.OperationOutcome oo = (org.hl7.fhir.r4.model.OperationOutcome) results
+                        .toOperationOutcome();
+                output = r4Context.newJsonParser().setPrettyPrint(true).encodeResourceToString(oo);
+                return new ResponseEntity<>(output, HttpStatus.OK);
+            } catch (DataFormatException e) {
+                logger.error("Exception in validateR4Resource");
+                org.hl7.fhir.r4.model.OperationOutcome outcomes = new org.hl7.fhir.r4.model.OperationOutcome();
+                outcomes.addIssue().setSeverity(org.hl7.fhir.r4.model.OperationOutcome.IssueSeverity.ERROR)
+                        .setDiagnostics("Failed to parse request body as JSON resource. Error was: " + e.getMessage());
+                output = r4Context.newJsonParser().setPrettyPrint(true).encodeResourceToString(outcomes);
+            }
+        }
+
+        logger.info("Before Return");
+        return new ResponseEntity<>(output, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
